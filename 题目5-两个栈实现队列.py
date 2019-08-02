192180d17b1d67a39f95f8e6fd512bcedd289336
LiShuan <48193287+MissLiShuan@users.noreply.github.com>
2019-08-02 08:21:37 +0800

    剑指offer

---
+++ 题目5-两个栈实现队列.py
@@ -0,0 +1,17 @@
+class Solution:
+    def __init__(self):
+        self.stackA = []
+        self.stackB = []
+    def push(self, node):
+        # write code here
+        self.stackA.append(node)
+    def pop(self):
+        # return xx
+        if self.stackB:
+            return self.stackB.pop()
+        elif not self.stackA:
+            return None
+        else:
+            while self.stackA:
+                self.stackB.append(self.stackA.pop())
+            return self.stackB.pop()
